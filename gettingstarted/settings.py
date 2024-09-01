cb6b262ef494e12455ba6adb2aaf85fb376e4aad
lbertalot <leandro.bertalot@gmail.com>
2024-09-01 12:24:07 -0300

    Implement JWT authentication

--- gettingstarted/settings.py
+++ gettingstarted/settings.py
@@ -69,9 +69,18 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "hello",
+    'rest_framework',
+    'rest_framework_simplejwt',
+    'hello',
 ]
-
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ],
+}
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     # Django doesn't support serving static assets in a production-ready way, so we use the
--- gettingstarted/views.py
+++ gettingstarted/views.py
@@ -1,5 +1,9 @@
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
 from django.http import JsonResponse
 
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
 def products(request):
     data = {
         "company": "Ualá",

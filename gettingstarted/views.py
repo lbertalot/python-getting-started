ab5fa12c11b14579b9de4427cbdefbeccbc67cb7
lbertalot <leandro.bertalot@gmail.com>
2024-10-07 21:12:35 -0300

    actiañizacion plazos fijos

--- gettingstarted/views.py
+++ gettingstarted/views.py
@@ -10,7 +10,7 @@ def products(request):
         "products": [
             {"productName": "Cuenta Remunerada", "value": "45%"},
             {"productName": "Fondo Común de Inversión", "value": "35.04%"},
-            {"productName": "Plazo Fijo", "value": "TNA 30 días: 37% | TNA 90 días: 40% | TNA 180 días: 45% | TEA 30 días: 43.98% | TEA 90 días: 46.45% | TEA 180 días: 50.14%"}
+            {"productName": "Plazo Fijo", "value": "TNA 30 días: 38% | TNA 90 días: 40% | TNA 180 días: 45% | TEA 30 días: 43.98% | TEA 90 días: 46.45% | TEA 180 días: 45% | TEA 365 días: 50%"}
         ]
     }
     return JsonResponse(data, json_dumps_params={'ensure_ascii': False})

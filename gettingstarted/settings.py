d38f96a8e9cbc04f7627aa7d5b289dd8b18fbca0
lbertalot <leandro.bertalot@gmail.com>
2024-12-15 20:28:24 -0300

    Fix database connection issue

--- gettingstarted/settings.py
+++ gettingstarted/settings.py
@@ -128,7 +128,7 @@ if IS_HEROKU_APP:
     # https://github.com/jazzband/dj-database-url
     DATABASES = {
         "default": dj_database_url.config(
-            env="JAWSDB_MARIA_URL",
+            env="JAWSDB_URL",
             conn_max_age=600,
             conn_health_checks=True,
             ssl_require=True,
